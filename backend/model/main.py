a31f56ca7657235f727fa2a9ef57d3f1ed22669c
Nikolaus Arbogast <nikolaus.arbogast@sva.de>
2024-11-26 14:44:13 +0100

    Change out model to model without HF access key

--- backend/model/main.py
+++ backend/model/main.py
@@ -5,7 +5,8 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
  
 # Modell und Tokenizer laden
-model_name = "meta-llama/Llama-3.2-3B-Instruct"
+model_name = "unsloth/Llama-3.2-3B-Instruct-bnb-4bit"
+#hf_token = os.getenv("HUGGINGFACE_TOKEN")
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
 model.to("cuda")
