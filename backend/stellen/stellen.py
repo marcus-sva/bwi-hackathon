761112ec7449b8d41498d7dedde12078be474e73
Leon Imhof <leon.imhof@mail.de>
2024-11-26 11:57:25 +0100

    parse id and job title from pdf

--- backend/stellen/stellen.py
+++ backend/stellen/stellen.py
@@ -1,4 +1,38 @@
 from tika import parser
+from pprint import pprint
+import re
 
-raw = parser.from_file('frontendDeveloper.pdf')
-print(raw['content'])
+
+def parse_pdf(pdf_filename):
+    """
+    parses STELLEN-ID, job title and pdf content from pdf file
+    """
+    raw = parser.from_file(pdf_filename)
+    metadata = raw['metadata']
+    #pprint(metadata)
+    content = raw['content']
+    stellen_id = parse_id(content)
+    stellen_title = parse_title(content)
+    print("STELLEN_ID: ", stellen_id)
+    print("STELLEN_TITLE: ", stellen_title)
+
+def parse_id(content):
+    stellen_id = re.search('(?:STELLEN.ID:\\s*)(\\d+)', content)
+    return stellen_id.group(1)
+
+def parse_title(content):
+    stellen_title = "No title"
+    content_lines = content.split('\n')
+    for line in content_lines:
+        if "Stellenanzeige" in line:
+            continue
+        if "STELLEN" in line:
+            continue
+        if len(line) > 10:
+            stellen_title = line
+            break
+    return stellen_title
+
+if __name__ == "__main__":
+    pdf_filename = 'frontendDeveloper.pdf'
+    parse_pdf(pdf_filename)
