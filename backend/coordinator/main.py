8051b2eb7fbd5cf3ada153ca75327fb1555211c1
Marcus Herz <marcus.herz@sva.de>
2024-11-27 12:43:48 +0100

    coordinator triggers cv.pdf to cv.json as concurrent coroutines

--- backend/coordinator/main.py
+++ backend/coordinator/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import io
 import json
 import os
@@ -19,7 +20,7 @@ def random_id():
     return random.randint(1, 1000)
 
 
-def cv_pdf_to_json(applicant_id: int, job_id: int):
+async def cv_pdf_to_json(applicant_id: int, job_id: int):
     return ""
 
 
@@ -59,7 +60,7 @@ async def upload_cv(job_id: int, file: UploadFile = File(...)):
         )
 
         # @TODO TRIGGER pdf to json generation via other service
-        cv_pdf_to_json(random_applicants_id, job_id)
+        asyncio.create_task(cv_pdf_to_json(random_applicants_id, job_id))
 
         # Return success response
         return {
